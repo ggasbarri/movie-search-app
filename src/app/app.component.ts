4a3846592dc2d0e63690dc9734df40c92ce79174
Gianfranco Gasbarri <ggasbarri@negocioselectronicos.biz>
2018-02-01 18:27:53 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { data } from '../assets/mock-data';
+import { Movie } from './Movie';
+import { MovieService } from './movie-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+  currentMovie: Movie = data[0];
+
+  constructor(private movieService: MovieService) {
+    movieService.currentMovie.subscribe(movie => {
+      this.currentMovie = movie;
+    });
+  }
 }
